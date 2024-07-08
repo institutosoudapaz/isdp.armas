3342dffb148a875d1f39b35fff319f800b2836d5
williamorim <thewilliam89@gmail.com>
2024-07-08 16:13:31 -0300

    tentando juntar tab de armas complementar

---
+++ data-raw/dados_rj/tab_armas_complementar.R
@@ -0,0 +1,61 @@
+# Verificar se é possível juntar as informações da tabela
+# "RJ 24648_APREENS_ARMA_FOGO_2019_BASE_APREENSAO.xlsx" com a tabela de armas
+# gerada
+
+dados_armas <- readr::read_rds("data-raw/dados_armas_rj.rds")
+dados_ocorrencias <- readr::read_rds("data-raw/dados_ocorrencias_rj.rds")
+dplyr::glimpse(dados_armas)
+dplyr::glimpse(dados_ocorrencias)
+
+dados_armas_complementar <- readxl::read_excel(
+  "data-raw/dados_rj/RJ 24648_APREENS_ARMA_FOGO_2019_BASE_APREENSAO.xlsx",
+  guess_max = 5000
+) |>
+  janitor::clean_names() |>
+  dplyr::filter(classe == "ARMA DE FOGO")
+
+dplyr::glimpse(dados_armas_complementar)
+
+# Por controle
+dados_armas_complementar |>
+  dplyr::count(numero_de_serie, sort = TRUE)
+
+controle <- dados_armas |>
+  dplyr::mutate(controle = stringr::str_sub(controle, 1, 7)) |>
+  dplyr::pull(controle)
+
+sum(controle %in% dados_armas_complementar$controle_interno_sco)
+
+# Por dados da ocorrência
+dados_armas |>
+  dplyr::left_join(
+    dados_ocorrencias |>
+      dplyr::mutate(
+        data_com = paste(data_com, hora_com)
+      ) |> 
+      dplyr::distinct(controle, data_com),
+    by = "controle"
+  ) |>
+  dplyr::left_join(
+    dados_armas_complementar |>
+      dplyr::select(
+        numero_de_serie,
+        tipo,
+        calibre,
+        marca,
+        data_com = data_registro
+      ) |>
+      dplyr::mutate(
+        dplyr::across(
+          c(tipo, calibre, marca),
+          tolower
+        ),
+        data_com = lubridate::with_tz(data_com, "America/Sao_Paulo"),
+        data_com = as.character(data_com)
+      ),
+    by = c("tipo", "calibre", "marca", "data_com")
+  ) |> 
+  dplyr::filter(!is.na(numero_de_serie))
+
+dados_armas_complementar |> 
+dplyr::filter(!is.na(numero_de_serie))
