4e70ebe10e20b8e814163945c189c7d759cd1d3c
williamorim <thewilliam89@gmail.com>
2024-07-26 14:18:34 -0300

    validacao num serie

--- data-raw/dados_sp/02-validacao.R
+++ data-raw/dados_sp/02-validacao.R
@@ -5,3 +5,14 @@ dados_sp <- readr::read_rds("inst/dados_sp/dados_sp.rds")
 dados_sp |> 
   dplyr::distinct(id_bo, ano_bo) |> 
   dplyr::count(ano_bo)
+
+
+# Número de série
+
+dados_sp |> 
+  dplyr::filter(!stringr::str_detect(numero_arma, "[0-9]")) |> 
+  dplyr::mutate(
+    arma_numero_serie = toupper(numero_arma)
+  ) |> 
+  dplyr::count(numero_arma, flag_arma_fogo, sort = TRUE) |> 
+  writexl::write_xlsx("data-raw/dados_sp/docs/validacao_numero_serie.xlsx")
