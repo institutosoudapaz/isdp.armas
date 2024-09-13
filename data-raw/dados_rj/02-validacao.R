1807963b6d155e05c054b3b5a899254addc0f676
williamorim <thewilliam89@gmail.com>
2024-09-13 11:48:05 -0300

    gerando crimes do rj

--- data-raw/dados_rj/02-validacao.R
+++ data-raw/dados_rj/02-validacao.R
@@ -10,31 +10,31 @@ dados_armas |>
   dplyr::count(ano)
 
 # controle
-dados_armas$controle |> 
-  nchar() |> 
+dados_armas$controle |>
+  nchar() |>
   unique()
 
 # todos os códigos de controle possuem 13 caracteres
 
 # categoria
-dados_armas |> 
+dados_armas |>
   dplyr::count(categoria)
 
 # só existe categoria = 1
 
 # tipo
-dados_armas |> 
+dados_armas |>
   dplyr::count(tipo)
 
 # 11 tipos de armas
 # existe garrucha e garruchão
 
 # calibre
-dados_armas |> 
+dados_armas |>
   dplyr::count(calibre, sort = TRUE)
 
-dados_armas |> 
-  dplyr::count(calibre, sort = TRUE) |> 
+dados_armas |>
+  dplyr::count(calibre, sort = TRUE) |>
   dplyr::filter(n == 1)
 
 
@@ -42,26 +42,26 @@ dados_armas |>
 # - 44 tipos com apenas 1 ocorrência
 
 # marca
-dados_armas |> 
+dados_armas |>
   dplyr::count(marca, sort = TRUE)
 
-dados_armas |> 
-  dplyr::count(marca, sort = TRUE) |> 
+dados_armas |>
+  dplyr::count(marca, sort = TRUE) |>
   dplyr::filter(n == 1)
 
 # - 157 tipos diferentes
 # - 26 tipos com apenas 1 ocorrência
-# - Existem vários nomes para NA: inderterminado, não identificada, 
+# - Existem vários nomes para NA: inderterminado, não identificada,
 #   não identificado, s/marca...
 
 # quantidade
-dados_armas |> 
+dados_armas |>
   dplyr::count(quantidade, sort = TRUE)
 
 # Com exceção de 2, com quantidade = 7, todos são quantidade = 1
 
 # origem
-dados_armas |> 
+dados_armas |>
   dplyr::count(origem, sort = TRUE)
 
 
@@ -72,8 +72,8 @@ dados_ocorrencias <- readr::read_rds("inst/dados_rj/dados_ocorrencias_rj.rds")
 dplyr::glimpse(dados_ocorrencias)
 
 # controle
-dados_ocorrencias$controle |> 
-  nchar() |> 
+dados_ocorrencias$controle |>
+  nchar() |>
   unique()
 
 # todos os códigos de controle possuem 13 caracteres
@@ -86,25 +86,62 @@ min(dados_ocorrencias$data_fato, na.rm = TRUE) # 1916
 max(dados_ocorrencias$data_fato, na.rm = TRUE) # 2023
 
 # sexo
-dados_ocorrencias |> 
+dados_ocorrencias |>
   dplyr::count(sexo)
 
 # cor
-dados_ocorrencias |> 
+dados_ocorrencias |>
   dplyr::count(cor)
 
 # escolaridade
-dados_ocorrencias |> 
+dados_ocorrencias |>
   dplyr::count(escolaridade)
 
 # profissao
-dados_ocorrencias |> 
+dados_ocorrencias |>
   dplyr::count(profissao)
 
 # relacao
-dados_ocorrencias |> 
+dados_ocorrencias |>
   dplyr::count(relacao)
 
 # conteudo
-dados_ocorrencias |> 
+dados_ocorrencias |>
   dplyr::count(conteudo)
+
+
+
+# Lista de crimes -----------------------------------------------------------
+
+dados_ocorrencias <- readr::read_rds("inst/dados_rj/dados_ocorrencias_rj.rds")
+dados_armas_complementar <- readr::read_rds("inst/dados_rj/dados_armas_complementar.rds")
+
+dplyr::bind_rows(
+  dados_ocorrencias |>
+    dplyr::select(titulo, titulo_do) |>
+    tidyr::pivot_longer(
+      cols = c(titulo, titulo_do),
+      names_to = "coluna_origem",
+      values_to = "crime"
+    ) |>
+    dplyr::distinct() |>
+    dplyr::mutate(base_origem = "dados_ocorrencias"),
+  dados_armas_complementar |>
+    dplyr::select(crime = tipo_delito) |>
+    tidyr::separate_longer_delim(
+      col = crime,
+      delim = "///"
+    ) |>
+    dplyr::mutate(
+      crime = stringr::str_squish(crime),
+      coluna_origem = "tipo_delito",
+      base_origem = "dados_armas_complementar"
+    ) |>
+    dplyr::distinct()
+) |> 
+dplyr::select(
+  base_origem,
+  coluna_origem,
+  crime
+) |> 
+  writexl::write_xlsx("data-raw/dados_rj/validacao/crimes_rj.xlsx")
