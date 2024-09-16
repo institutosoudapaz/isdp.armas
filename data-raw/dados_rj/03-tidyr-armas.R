9254534595f7639363361d77097d48715613c42a
williamorim <thewilliam89@gmail.com>
2024-09-16 16:26:18 -0300

    parcial tab armas

--- data-raw/dados_rj/03-tidyr-armas.R
+++ data-raw/dados_rj/03-tidyr-armas.R
@@ -35,31 +35,26 @@ dados_armas_consolidado <- dados_armas_formatado |>
       stringr::str_detect(tipo_arma_calibre, tipo_formatado) ~ FALSE,
       TRUE ~ TRUE
     ),
-    arma_tipo_join = dplyr::case_when(
+    arma_tipo_final = dplyr::case_when(
       tipo_formatado == "artesanal" ~ "artesanal",
       !flag_tipo_arma_incompativel_calibre ~ tipo_formatado,
       !is.na(tipo_arma_calibre) ~ tipo_arma_calibre,
       TRUE ~ tipo_formatado
     ),
-    arma_tipo_join = tolower(arma_tipo_join),
-    arma_tipo_join = stringr::str_squish(arma_tipo_join),
+    arma_tipo_final = tolower(arma_tipo_final),
+    arma_tipo_final = stringr::str_squish(arma_tipo_final),
     arma_calibre_final = dplyr::case_when(
       calibre_formatado_final == ".32 ou 34 G" &
-        tipo_formatado == "espingarda" ~ "32 gauge",
+        arma_tipo_final == "espingarda" ~ "32 gauge",
       calibre_formatado_final == ".32 ou 34 G" &
-        tipo_formatado %in% c("revolver", "garrucha") ~ ".32 S&W long",
+        arma_tipo_final %in% c("revolver", "garrucha") ~ ".32 S&W long",
       calibre_formatado_final == ".32 ou 34 G" &
-        tipo_formatado %in% c("pistola", "carabina", "submetralhadora") ~ ".32 acp",
+        arma_tipo_final %in% c("pistola", "carabina", "submetralhadora") ~ ".32 acp",
       stringr::str_detect(calibre_formatado_final, "32$") ~ ".32 S&W long",
       calibre_formatado_final == ".32 ou 34 G" ~ ".32",
       TRUE ~ calibre_formatado_final
     ),
     arma_calibre_final = toupper(arma_calibre_final),
-    arma_calibre_join = ifelse(
-      stringr::str_detect(arma_calibre_final, "[.](32|22|38) "),
-      stringr::str_sub(arma_calibre_final, 1, 3),
-      arma_calibre_final
-    ),
     arma_marca_final = dplyr::case_when(
       stringr::str_detect(tolower(arma_marca), "taur") ~ "taurus",
       stringr::str_detect(tolower(arma_marca), "ross") ~ "rossi",
@@ -67,7 +62,7 @@ dados_armas_consolidado <- dados_armas_formatado |>
       stringr::str_detect(tolower(arma_marca), "smith") ~ "s&w",
       TRUE ~ tolower(marca_arma_v2)
     ),
-    flag_arma_artesanal = stringr::str_detect(arma_tipo_join, "artesanal"),
+    flag_arma_artesanal = stringr::str_detect(arma_tipo_final, "artesanal"),
     flag_arma = as.logical(flag_arma),
     flag_arma = dplyr::case_when(
       flag_arma ~ TRUE,
@@ -78,7 +73,7 @@ dados_armas_consolidado <- dados_armas_formatado |>
     id_arma = vctrs::vec_group_id(
       paste(
         controle,
-        arma_tipo_join,
+        arma_tipo_final,
         arma_calibre_final,
         arma_marca_final,
         arma_origem,
@@ -108,19 +103,22 @@ armas_final <- dados_armas_consolidado |>
   dplyr::select(
     id_bo = controle,
     id_arma,
-    arma_calibre_original = arma_calibre,
-    arma_marca_original = arma_marca,
-    arma_tipo_original = arma_tipo,
-    arma_marca_final = arma_marca_final,
-    pais_fabricacao,
-    compatibilidade_tipo = tipo_arma_calibre,
-    arma_tipo_join,
-    tipo_formatado,
-    dplyr::contains("final"),
-    arma_origem,
     flag_arma,
+    arma_tipo_original = arma_tipo,
+    arma_tipo_formatado = tipo_formatado,
+    arma_tipo_final,
+    flag_arma_artesanal,
+    arma_calibre_original = arma_calibre,
+    arma_calibre_formatado = calibre_formatado_final,
+    arma_calibre_final,
+    compatibilidade_tipo = tipo_arma_calibre,
     flag_tipo_arma_incompativel_calibre,
-    flag_arma_artesanal
+    arma_marca_original = arma_marca,
+    arma_marca_formatado = marca_arma_v2,
+    arma_marca_final,
+    pais_fabricacao,
+    arma_origem,
+    flag_arma_policial
   )
 
 # Gerando arquivo
@@ -132,3 +130,24 @@ writexl::write_xlsx(
 )
 
 
+# Validação
+
+armas_final |> 
+  dplyr::filter(is.na(arma_marca_final), !is.na(arma_marca_original)) |> 
+  dplyr::count(
+    arma_marca_original,
+    arma_marca_final,
+    sort = TRUE
+  ) |> 
+  writexl::write_xlsx("data-raw/dados_rj/validacao/escape_marcas.xlsx")
+
+
+armas_final |> 
+  dplyr::filter(is.na(arma_calibre_final), !is.na(arma_calibre_original)) |> 
+  dplyr::count(
+    arma_calibre_original,
+    compatibilidade_tipo,
+    arma_calibre_final,
+    sort = TRUE
+  ) |>
+  writexl::write_xlsx("data-raw/dados_rj/validacao/escape_calibre.xlsx")
