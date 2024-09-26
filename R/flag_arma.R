ea7bb2c33a814537585beb5374d6dfcb3917e7d2
williamorim <thewilliam89@gmail.com>
2024-09-25 22:42:42 -0300

    ajustando funcao flag arma

--- R/flag_arma.R
+++ R/flag_arma.R
@@ -5,16 +5,29 @@ gerar_flag_arma_artesanal <- function(tab) {
     )
 }
 
-gerar_flag_arma <- function(tab) {
-  tab |>
-    dplyr::mutate(
-      flag_arma = as.logical(flag_arma),
-      flag_arma = dplyr::case_when(
-        flag_arma ~ TRUE,
-        stringr::str_detect(tolower(arma_numero_serie), "simulacro|muni[çc][ãa]o|chumbo") ~ FALSE,
-        stringr::str_detect(tolower(arma_calibre), "cbc") ~ FALSE,
-        arma_marca_final == "cbc" ~ FALSE,
-        TRUE ~ flag_arma
+gerar_flag_arma <- function(tab, base) {
+  if (base %in% c("sp", "rj_complementar")) {
+    tab |>
+      dplyr::mutate(
+        flag_arma = as.logical(flag_arma),
+        flag_arma = dplyr::case_when(
+          flag_arma ~ TRUE,
+          stringr::str_detect(tolower(arma_numero_serie), "simulacro|muni[çc][ãa]o|chumbo") ~ FALSE,
+          stringr::str_detect(tolower(arma_calibre), "cbc") ~ FALSE,
+          arma_marca_final == "cbc" ~ FALSE,
+          TRUE ~ flag_arma
+        )
       )
-    )
+  } else if(base == "rj") {
+    tab |>
+      dplyr::mutate(
+        flag_arma = as.logical(flag_arma),
+        flag_arma = dplyr::case_when(
+          flag_arma ~ TRUE,
+          stringr::str_detect(tolower(arma_calibre), "cbc") ~ FALSE,
+          arma_marca_final == "cbc" ~ FALSE,
+          TRUE ~ flag_arma
+        )
+      )
+  }
 }
