acc667c07d4d1a930ccbdc9f2770fc5ed0921139
Fernando Poliano <fptcorrea@gmail.com>
2024-10-08 17:46:35 -0300

    centroides

---
+++ data-raw/dados_sp/05-centroides.R
@@ -0,0 +1,25 @@
+shape <- readRDS("shp_capital.rds")
+
+library(sf)
+
+centroides <- st_centroid(shape$geometry)
+
+processa_centroide <- function(centro){
+  tibble::tibble(
+    lat = centro[[1]],
+    lon = centro[[2]]
+  )
+}
+
+tabela_dados_dps <- tibble::tibble(
+  DpGeoDes = shape$DpGeoDes,
+  SecGeoDes = shape$SecGeoDes,
+  DepGeoDes = shape$DepGeoDes
+)
+
+centros_latlon <- tabela_dados_dps |>
+  dplyr::bind_cols(
+    purrr::map_dfr(centroides, processa_centroide)
+  )
+
+saveRDS(centros_latlon, "inst/dados_sp/centroides_dps.rds")
